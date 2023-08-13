0010560277067828f6b0fb12b1ce5076fb880c78
Fabrizio Pio Caponio <fabrizio.caponio99@gmail.com>
2023-08-13 20:11:19 +0200

    aggiusti

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -34,7 +34,7 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
           onClick={handleProfileClick}
         >
           <Image
-            src={post?.creator?.image}
+            src={post.creator.image}
             alt='user_image'
             width={40}
             height={40}
@@ -43,10 +43,10 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
 
           <div className='flex flex-col'>
             <h3 className='font-satoshi font-semibold text-gray-900'>
-              {post?.creator?.username}
+              {post.creator.username}
             </h3>
-            <p className='font-inter text-sm text-gray-500 hidden'>
-              {post?.creator?.email}
+            <p className='font-inter text-sm text-gray-500'>
+              {post.creator.email}
             </p>
           </div>
         </div>
@@ -73,19 +73,19 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
         #{post.tag}
       </p>
 
-      {session?.user?.id === post?.creator?._id && pathName === "/profile" && (
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
         <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
           <p
-            className='font-inter text-sm green_gradient cursor-pointer hover:scale-90 duration-300'
+            className='font-inter text-sm green_gradient cursor-pointer'
             onClick={handleEdit}
           >
-            Modifica
+            Edit
           </p>
           <p
-            className='font-inter text-sm text-red-600 hover:scale-90 duration-300 gradient-to-tr from-red-300 to-red-900 cursor-pointer'
+            className='font-inter text-sm orange_gradient cursor-pointer'
             onClick={handleDelete}
           >
-            Cancella
+            Delete
           </p>
         </div>
       )}
